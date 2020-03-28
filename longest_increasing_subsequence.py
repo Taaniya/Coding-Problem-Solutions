3083322c95872fed965164a8a9d7651ad7322cd5
TaaniyaArora <taaniyarora@gmail.com>
2020-03-29 02:57:33 +0530

    Update longest_increasing_subsequence.py

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -2,8 +2,7 @@
 
 def getLis(arr):
     """
-    Find length of longest increasing subsequence using Dynamic Programming - Memoization
-    
+    Find length of longest increasing subsequence using Dynamic Programming - Memoization.
     Parameters :
     -------------------
     arr = List(int)
@@ -11,38 +10,33 @@ def getLis(arr):
     Returns :
     -------------------
     maximum = int 
-    
     eg.
     >>> getLis([10, 22, 9, 33, 21, 50, 41, 60])
     >>> 5
     
     >>> getLis([10, 22, 33, 15, 5, 50])
     >>> 4
-    
     """      
+    size = len(arr)
+    memo = [1] * size
+    done = [0] * size
     maximum = 1
-    memo = [1] * (len(arr))
-    done = [0] * (len(arr))
-
-    def lis(arr):   
+    
+    def lis(i,j):   
         nonlocal maximum
-        size = len(arr)
-        if size == 1:
+        if done[i]:
+            return memo[i]
+        elif j < 0:
             return 1
-        
         else:
-            for j in range(size - 1 ):
-                if not done[j]:
-                    memo[j] = lis(arr[:j+1])
-                    done[j] = 1
-                    maximum = max(maximum, memo[j])
-
-                if arr[size-1] > arr[j]:
-                    memo[size-1] = max( memo[j] + 1, memo[size-1])
-                    maximum = max(maximum, memo[size-1])
+            for j in range(i):                
+                if arr[i] > arr[j]:
+                    memo[i] = max( lis(j,j-1) + 1, memo[i])
+        maximum = max(maximum, memo[i])
+        done[i] = 1        
+        return memo[i]
+    
+    for i in range(size):
+        _ = lis(i, i-1)
         
-        done[size-1] = 1        
-        return memo[size-1]
-    _ = lis(arr)
     return maximum
-
