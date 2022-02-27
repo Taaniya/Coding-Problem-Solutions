23bd2adf494340038732946c9189f2b75ee5183c
Taaniya Arora <taaniyarora@gmail.com>
2022-02-28 02:01:57 +0530

    create binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,50 @@
+class BSTNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+     
+ def insert(root, val):
+  if root is None:
+     root = BSTNode(val) 
+  elif val < root.data:
+     root.left = insert(root.left, val)
+  elif val > root.data:
+     root.right = insert(root.right, val)
+  elif val == root.data:
+    print(f"Node with given data already exists. Can't insert duplicates. val={val}, root.data {root.data}")
+  return root
+
+def preorder(root):
+  if root is not None:
+    print(root.data)
+    preorder(root.left)
+    preorder(root.right)
+  return
+
+def inorder(root):
+  if root is not None:
+    inorder(root.left)
+    print(root.data)
+    inorder(root.right)
+  return
+
+def postorder(root):
+  if root is not None:
+    postorder(root.left)
+    postorder(root.right)
+    print(root.data)
+  return
+
+if __name__ == "__main__":
+  treenode = BSTNode(5)
+  keys = [4,3,8,7,10]
+  for k in keys:
+    _ = insert(treenode, k)
+  print("\nInorder traversal")
+  inorder(treenode)
+  print("\nPreorder traversal")
+  preorder(treenode)
+  print("\nPostorder traversal")
+  postorder(treenode)
+  
