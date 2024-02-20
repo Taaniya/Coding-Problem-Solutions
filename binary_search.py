77188f286a903d5f377ddd69e21b14f626a6bc43
Taaniya Arora <taaniyarora@gmail.com>
2024-02-21 01:37:28 +0530

    Added alternative iterative algo for binary_search

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,8 @@
 #!/bin/env python
 
-def binary_search(ls, x):
+from typing import List
+
+def binary_search(ls: List[float], x: float) -> int:
   """ This function performs binary search for a given element in given array
   sorted in ascending order. 
   Time complexity - O(logn)
@@ -23,3 +25,20 @@ def binary_search(ls, x):
       old_mid = mid
       mid = low + (high-low) // 2
   return -1
+
+
+def binary_search(arr: List[float], ele: float) -> int:
+    mid_idx = len(arr) // 2          
+    st_idx = 0
+    end_idx = len(arr) - 1 
+
+    while((mid_idx >= st_idx) & (mid_idx <= end_idx)):
+        if ele == arr[mid_idx]:          # found
+            return mid_idx
+        elif ele < arr[mid_idx]:
+            end_idx = mid_idx - 1
+            mid_idx = (st_idx + end_idx ) // 2
+        elif ele > arr[mid_idx]:
+            st_idx = mid_idx + 1
+            mid_idx = (st_idx + end_idx ) // 2
+    return -1             # not found 
