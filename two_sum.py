ae7e6c1956472d43c5c24819cd44a62f3a0716c9
Taaniya Arora <taaniyarora@gmail.com>
2024-03-22 14:51:17 +0530

    Added solution for two_sum problem

---
+++ two_sum.py
@@ -0,0 +1,35 @@
+"""
+Problem statement: 
+----------------------------------------------------
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+E.g., 
+1. Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
+2. Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+-----------------------------------------------------
+"""
+
+import collections
+
+class Solution:    
+    def twoSum(self, nums, target):
+        lookup = collections.defaultdict(set)
+        for i,num in enumerate(nums):
+            lookup[num].add(i)             # map num and its index
+            complement = target - num
+            if complement in lookup:
+                for comp_id in lookup[complement]:
+                    if comp_id != i:
+                        return [i,comp_id]
+        return []        
+        
