4ba64f30f15b6bfafc94afa24192e9260958fd0d
Taaniya Arora <taaniyarora@gmail.com>
2024-09-09 03:03:15 +0530

    Update detonate_max_bombs.py

--- detonate_max_bombs.py
+++ detonate_max_bombs.py
@@ -15,6 +15,9 @@ Output: 1
 
 Input: bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]]
 Output: 5
+
+Input: bombs = [[4,4,3],[4,4,3]]
+Output: 2
 """
 from typing import List, Tuple
 from collections import defaultdict, deque
