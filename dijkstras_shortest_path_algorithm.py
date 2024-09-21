9ea2fc8e84e1c4735fdd63785f83bb6654fb2393
Taaniya Arora <taaniyarora@gmail.com>
2024-09-22 03:22:21 +0530

    Update dijkstras_shortest_path_algorithm.py

--- dijkstras_shortest_path_algorithm.py
+++ dijkstras_shortest_path_algorithm.py
@@ -115,10 +115,12 @@ class Graph():
 
     def dijkstra(self, src):
         """
-        Implements Dijkstra's algorithm to compute shortest path from a source node to all other nodes in the graph
-        represented as edglist. This implementation uses min-Priority queue using Binary Heap and calls three priority queue operations
+        - Implements Dijkstra's algorithm to compute shortest path from a source node to all other nodes in the graph
+        represented as edglist. 
+        - This implementation uses min-Priority queue using Binary Heap and calls three priority queue operations
         insert, extract-min & decrease-key (implicit while relaxing).
-
+        - Min-heap based priority queue is used to maintain distances of each graph vertex from the given source vertex.
+        
         Parameters:
         --------------
         src - index of source node
@@ -147,6 +149,7 @@ class Graph():
         dist = []  # hold shortest distance from source for each node in the graph
         source_nbrs = dict([(v_id, dist) for v_id,dist in self.graph[src]])
 
+        # Step 1: Initialize priority queue with distance of each vertex from given source
         for v_id in range(0, self.v):
             if v_id != src:
                 if v_id in source_nbrs:   # if vertices are directly forming any edge with src, include their distance from source
@@ -160,7 +163,8 @@ class Graph():
             else:
                 unvisited.insert((0, src))
                 dist.append(0)
-        # Perform BFS traversal
+        
+        # Step 2: Perform BFS traversal & update and find shortest distance for each vertex from source along the way
         while unvisited.queue:
             min_node = unvisited.extract_min()
             visited.add(min_node)
