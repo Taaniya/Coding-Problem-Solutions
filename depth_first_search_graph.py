ebecf330ca8d323058d2d1a6076c4e8bc50d3ccf
Taaniya Arora <taaniyarora@gmail.com>
2024-09-23 12:11:00 +0530

    Update depth_first_search_graph.py

--- depth_first_search_graph.py
+++ depth_first_search_graph.py
@@ -11,13 +11,14 @@ class Graph():
         self.graph[u].add(v)
 
     def DFS(self, src):
-        '''Performs depth-first search with cycle detection for a directed unweighted graph represented as edgelist format.
+        '''Performs depth-first search with cycle detection for a directed (or undirected) unweighted graph represented as edgelist format. 
+        https://stackoverflow.com/questions/2869647/why-dfs-and-not-bfs-for-finding-cycle-in-graphs
         Time complexity: O(V+E) 
         '''
         visited = [0] * self.v
         in_progress = set()               # nodes currently explored with DFS
         def visitNode(src):
-            in_progress.add(src)          # keep track of parent & ancestors visited (in case of undirected graph)
+            in_progress.add(src)          
             visited[src] = 1
             for nbr in self.graph[src]:
                 if not visited[nbr]:
